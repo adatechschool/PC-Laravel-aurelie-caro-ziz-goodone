9cf3e2ab8070ac1e6dffd9c31d9c187a3bd79e92
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2021-11-29 14:57:03 +0100

    tentative d'ajout d'app

---
+++ example-app/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+use Illuminate\Routing\Controller as BaseController;
+
+class ArticleController extends BaseController
+
+{   
+    public function index(){
+        
+    }
+    public function show(Article $article){
+    return view('articles.show', [
+        'article' => $article
+    ]);
+}
+}
